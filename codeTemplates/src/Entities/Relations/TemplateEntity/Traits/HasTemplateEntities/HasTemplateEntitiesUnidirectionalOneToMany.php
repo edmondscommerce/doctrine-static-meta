1cfbc9fec0a9ed790e6d835110fab77595ed4e63
Joseph Edmonds <joseph@edmondscommerce.co.uk>
2018-02-06 10:40:59 +0000

    correcting method naming

--- codeTemplates/src/Entities/Relations/TemplateEntity/Traits/HasTemplateEntities/HasTemplateEntitiesUnidirectionalOneToMany.php
+++ codeTemplates/src/Entities/Relations/TemplateEntity/Traits/HasTemplateEntities/HasTemplateEntitiesUnidirectionalOneToMany.php
@@ -24,7 +24,7 @@ trait HasTemplateEntitiesUnidirectionalOneToMany
      *
      * @throws \EdmondsCommerce\DoctrineStaticMeta\Exception\DoctrineStaticMetaException
      */
-    public static function getPropertyMetaForTemplateEntity(ClassMetadataBuilder $builder): void
+    public static function getPropertyMetaForTemplateEntities(ClassMetadataBuilder $builder): void
     {
         $manyToManyBuilder = $builder->createManyToMany(
             TemplateEntity::getPlural(),
