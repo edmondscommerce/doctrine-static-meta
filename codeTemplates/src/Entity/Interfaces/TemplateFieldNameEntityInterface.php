f9ea018b481f7b9a6dba635137bcdd3baccf52d5
Ben Rowan <ben@edmondscommerce.co.uk>
2018-02-27 14:05:42 +0000

    Add interface template

---
+++ codeTemplates/src/Entity/Interfaces/TemplateFieldNameEntityInterface.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace TemplateNamespace\Entity\Interfaces;
+
+use EdmondsCommerce\DoctrineStaticMeta\Entity as DSM;
+
+interface TemplateFieldNameEntityInterface extends DSM\Interfaces\EntityInterface
+{
+
+}
