9fc96d3f180a48319ad84dbf6ab316af7e48cc35
Ben Rowan <ben@edmondscommerce.co.uk>
2018-03-20 15:05:45 +0000

    Update GenerateFieldCommandTest to use namespaced fields

--- tests/CodeGeneration/Command/GenerateFieldCommandTest.php
+++ tests/CodeGeneration/Command/GenerateFieldCommandTest.php
@@ -2,6 +2,7 @@
 
 namespace EdmondsCommerce\DoctrineStaticMeta\CodeGeneration\Command;
 
+use Doctrine\Common\Util\Inflector;
 use EdmondsCommerce\DoctrineStaticMeta\AbstractTest;
 use EdmondsCommerce\DoctrineStaticMeta\CodeGeneration\Generator\AbstractGenerator;
 use EdmondsCommerce\DoctrineStaticMeta\Exception\DoctrineStaticMetaException;
@@ -18,31 +19,28 @@ class GenerateFieldCommandTest extends AbstractCommandTest
      */
     public function testGenerateEntity()
     {
-        $command = $this->container->get(GenerateFieldCommand::class);
-        $tester  = $this->getCommandTester($command);
+        $command    = $this->container->get(GenerateFieldCommand::class);
+        $tester     = $this->getCommandTester($command);
         $fieldsPath = self::WORK_DIR.'src/Entity/Fields';
+        $namespace  = self::TEST_PROJECT_ROOT_NAMESPACE . AbstractGenerator::ENTITY_FIELD_TRAIT_NAMESPACE;
 
         $createdFiles = [];
 
         foreach (MappingHelper::COMMON_TYPES as $type) {
+            $classy   = Inflector::classify($type);
+            $fieldFqn = $namespace . "\\$classy\\$classy";
             $tester->execute(
                 [
                     '-'.GenerateFieldCommand::OPT_PROJECT_ROOT_PATH_SHORT      => self::WORK_DIR,
                     '-'.GenerateFieldCommand::OPT_PROJECT_ROOT_NAMESPACE_SHORT => self::TEST_PROJECT_ROOT_NAMESPACE,
-                    '-'.GenerateFieldCommand::OPT_FQN_SHORT                    => $type,
+                    '-'.GenerateFieldCommand::OPT_FQN_SHORT                    => $fieldFqn,
                     '-'.GenerateFieldCommand::OPT_TYPE_SHORT                   => $type
                 ]
             );
 
-            $createdFiles[] = "$fieldsPath/Interfaces/";
-            $createdFiles[] = "$fieldsPath/Traits/";
+            $createdFiles[] = "$fieldsPath/Interfaces/$classy/{$classy}FieldInterface.php";
+            $createdFiles[] = "$fieldsPath/Traits/$classy/{$classy}FieldTrait.php";
         }
-
-
-//        $createdFiles = [
-//            $this->entitiesPath.'/This/Is/A/TestEntity.php',
-//            $this->entitiesPath.'/../../tests/Entities/This/Is/A/TestEntityTest.php',
-//        ];
 
         foreach ($createdFiles as $createdFile) {
             $this->assertNoMissedReplacements($createdFile);
