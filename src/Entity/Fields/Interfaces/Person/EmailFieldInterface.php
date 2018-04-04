fc449e869332bc40f8ade49b331db1c3ea54c09f
Ben Rowan <ben@edmondscommerce.co.uk>
2018-04-04 10:26:25 +0000

    Add email field

---
+++ src/Entity/Fields/Interfaces/Person/EmailFieldInterface.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace EdmondsCommerce\DoctrineStaticMeta\Entity\Fields\Interfaces\Person;
+
+interface EmailFieldInterface
+{
+    public const PROP_EMAIL = 'email';
+
+    public function getEmail(): ?string;
+
+    public function setEmail(?string $email): EmailFieldInterface;
+}
