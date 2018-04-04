cb9a8c767822c7dd09dd0f74c04dd2172bfe4b4e
Ben Rowan <ben@edmondscommerce.co.uk>
2018-04-04 10:17:34 +0000

    Add qty field

---
+++ src/Entity/Fields/Interfaces/Attribute/QtyFieldInterface.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace EdmondsCommerce\DoctrineStaticMeta\Entity\Fields\Interfaces\Attribute;
+
+interface QtyFieldInterface
+{
+    public const PROP_QTY = 'qty';
+
+    public function getQty(): ?int;
+
+    public function setQty(?int $qty): QtyFieldInterface;
+}
---
+++ src/Entity/Fields/Traits/Attribute/QtyFieldTrait.php
@@ -0,0 +1,61 @@
+<?php declare(strict_types=1);
+
+namespace EdmondsCommerce\DoctrineStaticMeta\Entity\Fields\Traits\Attribute;
+// phpcs:disable
+
+use \Doctrine\ORM\Mapping\Builder\ClassMetadataBuilder;
+use \EdmondsCommerce\DoctrineStaticMeta\MappingHelper;
+use EdmondsCommerce\DoctrineStaticMeta\Entity\Fields\Interfaces\Attribute\QtyFieldInterface;
+use Symfony\Component\Validator\Constraints\GreaterThanOrEqual;
+use Symfony\Component\Validator\Mapping\ClassMetadata as ValidatorClassMetaData;
+
+// phpcs:enable
+trait QtyFieldTrait {
+
+	/**
+	 * @var int|null
+	 */
+	private $qty;
+
+	/**
+	 * @SuppressWarnings(PHPMD.StaticAccess) 
+	 */
+	public static function getPropertyDoctrineMetaForQty(ClassMetadataBuilder $builder) {
+		MappingHelper::setSimpleIntegerFields(
+		            [QtyFieldInterface::PROP_QTY],
+		            $builder,
+		            true
+		        );
+	}
+
+    /**
+     * @param ValidatorClassMetaData $metadata
+     *
+     * @throws \Symfony\Component\Validator\Exception\MissingOptionsException
+     * @throws \Symfony\Component\Validator\Exception\InvalidOptionsException
+     * @throws \Symfony\Component\Validator\Exception\ConstraintDefinitionException
+     */
+    protected static function getPropertyValidatorMetaForQty(ValidatorClassMetaData $metadata): void
+    {
+        $metadata->addPropertyConstraint(
+            QtyFieldInterface::PROP_QTY,
+            new GreaterThanOrEqual(0)
+        );
+    }
+
+	/**
+	 * @return int|null
+	 */
+	public function getQty(): ?int {
+		return $this->qty;
+	}
+
+	/**
+	 * @param int|null $qty
+	 * @return $this|QtyFieldInterface
+	 */
+	public function setQty(?int $qty) {
+		$this->qty = $qty;
+		return $this;
+	}
+}
