31da21b8c96b01eb53597c1f567edb87c8a4a31f
Ross Mitchell <ross@edmondscommerce.co.uk>
2018-04-17 14:05:35 +0100

    May have made a mistake with the csignore comments

--- codeTemplates/src/Entity/Repositories/AbstractEntityRepository.php
+++ codeTemplates/src/Entity/Repositories/AbstractEntityRepository.php
@@ -1,7 +1,7 @@
 <?php declare(strict_types=1);
 
 namespace TemplateNamespace\Entity\Repositories;
-// phpcs:disable - line length
+// phpcs:disable -- line length
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\Mapping;
