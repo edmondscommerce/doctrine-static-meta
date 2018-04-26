cc223fbfcc54d57394438692ec2d82f38b7da662
Ben Rowan <ben@edmondscommerce.co.uk>
2018-04-26 08:28:56 +0000

    Build example entities

---
+++ example/symfony/src/Entity/Repositories/ClientRepository.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace My\Test\Project\Entity\Repositories;
+
+use My\Test\Project\Entity\Repositories\AbstractEntityRepository;
+// phpcs:disable -- line length
+class ClientRepository extends AbstractEntityRepository
+{
+// phpcs: enable
+}
---
+++ example/symfony/src/Entity/Savers/AbstractSaver.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types=1);
+
+namespace My\Test\Project\Entity\Savers;
+// phpcs:disable
+
+// phpcs:enable
+class AbstractSaver extends \EdmondsCommerce\DoctrineStaticMeta\Entity\Savers\AbstractSaver {
+}
