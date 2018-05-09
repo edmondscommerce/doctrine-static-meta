1387e0a03a0dbfa0380c7d1b783be5306b23315f
Ross Mitchell <ross@edmondscommerce.co.uk>
2018-05-09 18:43:15 +0100

    Testing for the exception that is now being thrown

--- tests/CodeGeneration/Command/GenerateFieldMultipleTimesTest.php
+++ tests/CodeGeneration/Command/GenerateFieldMultipleTimesTest.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 /**
  * @category EdmondsCommerce
  * @package  EdmondsCommerce_
@@ -10,13 +10,14 @@ namespace EdmondsCommerce\DoctrineStaticMeta\CodeGeneration\Command;
 use Doctrine\Common\Util\Inflector;
 use EdmondsCommerce\DoctrineStaticMeta\AbstractTest;
 use EdmondsCommerce\DoctrineStaticMeta\CodeGeneration\Generator\AbstractGenerator;
+use EdmondsCommerce\DoctrineStaticMeta\Exception\DoctrineStaticMetaException;
 use EdmondsCommerce\DoctrineStaticMeta\MappingHelper;
 use Symfony\Component\Console\Tester\CommandTester;
 
 class GenerateFieldMultipleTimesTest extends AbstractCommandTest
 {
 
-    public const WORK_DIR = AbstractTest::VAR_PATH.'/GenerateSameFieldMultipleTime/';
+    public const WORK_DIR = AbstractTest::VAR_PATH . '/GenerateSameFieldMultipleTime/';
     /**
      * @var array
      */
@@ -24,73 +25,46 @@ class GenerateFieldMultipleTimesTest extends AbstractCommandTest
     /**
      * @var CommandTester
      */
-    private $commandTester;
-
     private $fieldGenerator;
 
     /**
      * @throws \EdmondsCommerce\DoctrineStaticMeta\Exception\DoctrineStaticMetaException
      */
-    public function setup()
+    public function setup(): void
     {
         parent::setup();
         $this->entityName     = $this->generateEntities();
         $generateCommand      = $this->container->get(GenerateFieldCommand::class);
         $this->fieldGenerator = $this->getCommandTester($generateCommand);
-        $command              = $this->container->get(SetFieldCommand::class);
-        $this->commandTester  = $this->getCommandTester($command);
     }
 
-
-    public function testGeneratingAFieldMultipleTimesWillProduceValidCode()
+    public function testItShouldNotBePossibleToGenerateTheSameFieldTwice(): void
     {
-        $type           = MappingHelper::TYPE_STRING;
-        $fieldName      = $this->getNameSpace('should_not_error');
-        $differentField = $this->getNameSpace('different_field');
+        $type      = MappingHelper::TYPE_STRING;
+        $fieldName = $this->getNameSpace('should_not_error');
         /* Generate the field */
         $this->generateField($fieldName, $type);
         /* And Again */
+        $this->expectException(DoctrineStaticMetaException::class);
         $this->generateField($fieldName, $type);
-        /* Once More For Good Luck */
-        $this->generateField($fieldName, $type);
-        /* We need to actually load the entity to check if it is valid so add another field */
-        $this->generateField($differentField, $type);
-        /* Now set the relationship */
-        $entityName = array_pop($this->entityName);
-        $this->setField($fieldName, $entityName);
-        $secondsEntityName = array_pop($this->entityName);
-        $this->setField($fieldName, $secondsEntityName);
-        $this->setField($differentField, $entityName);
-        $this->qaGeneratedCode();
-
     }
 
-    private function getNameSpace($fieldName)
+    private function getNameSpace($fieldName): string
     {
         $classy    = Inflector::classify($fieldName);
-        $namespace = static::TEST_PROJECT_ROOT_NAMESPACE.AbstractGenerator::ENTITY_FIELD_TRAIT_NAMESPACE;
+        $namespace = static::TEST_PROJECT_ROOT_NAMESPACE . AbstractGenerator::ENTITY_FIELD_TRAIT_NAMESPACE;
 
         return "$namespace\\$classy\\$classy";
     }
 
-    private function generateField($fullyQualifiedName, $type)
+    private function generateField($fullyQualifiedName, $type): void
     {
-        return $this->fieldGenerator->execute(
+        $this->fieldGenerator->execute(
             [
-                '-'.GenerateFieldCommand::OPT_PROJECT_ROOT_PATH_SHORT      => self::WORK_DIR,
-                '-'.GenerateFieldCommand::OPT_PROJECT_ROOT_NAMESPACE_SHORT => self::TEST_PROJECT_ROOT_NAMESPACE,
-                '--'.GenerateFieldCommand::OPT_FQN                         => $fullyQualifiedName,
-                '--'.GenerateFieldCommand::OPT_TYPE                        => $type,
-            ]
-        );
-    }
-
-    private function setField($fieldName, $entityName)
-    {
-        $this->commandTester->execute(
-            [
-                '-'.SetFieldCommand::OPT_FIELD_SHORT  => $fieldName.'FieldTrait',
-                '-'.SetFieldCommand::OPT_ENTITY_SHORT => $entityName,
+                '-' . GenerateFieldCommand::OPT_PROJECT_ROOT_PATH_SHORT      => self::WORK_DIR,
+                '-' . GenerateFieldCommand::OPT_PROJECT_ROOT_NAMESPACE_SHORT => self::TEST_PROJECT_ROOT_NAMESPACE,
+                '--' . GenerateFieldCommand::OPT_FQN                         => $fullyQualifiedName,
+                '--' . GenerateFieldCommand::OPT_TYPE                        => $type,
             ]
         );
     }
