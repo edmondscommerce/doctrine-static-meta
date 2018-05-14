a34818fbffc11a01b7f7e594055670df559c3626
Joseph Edmonds <joseph@edmondscommerce.co.uk>
2018-05-14 17:01:57 +0000

    an alternative to using the super global

--- example/symfony/project/src/Kernel.php
+++ example/symfony/project/src/Kernel.php
@@ -61,11 +61,11 @@ class Kernel extends BaseKernel
      */
     protected function addDsmServices(ContainerBuilder $container)
     {
-//        $config = [];
-//        foreach (Config::PARAMS as $param) {
-//            $config[$param] = \getenv($param);
-//        }
-        (new DsmContainer())->addConfiguration($container, $_SERVER);
+        $config = [];
+        foreach (Config::PARAMS as $param) {
+            $config[$param] = \getenv($param);
+        }
+        (new DsmContainer())->addConfiguration($container, $config);
     }
 
     protected function configureRoutes(RouteCollectionBuilder $routes)
