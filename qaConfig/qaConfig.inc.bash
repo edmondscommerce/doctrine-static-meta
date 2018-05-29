8859407d1e1ff8d2e6eb2fe7498e8ffe1b8e7b28
Joseph Edmonds <joseph@edmondscommerce.co.uk>
2018-05-29 14:02:33 +0000

    updated composer, totally disabling infection for now

--- qaConfig/qaConfig.inc.bash
+++ qaConfig/qaConfig.inc.bash
@@ -1,2 +1,7 @@
-#Due to requiring to track random values, we can only run tests in a single thread
+# Infection Configs
+
+## Due to requiring to track random values, we can only run tests in a single thread
 numberOfCores=1
+
+## Totally Disabling Infection for now
+useInfection=0
