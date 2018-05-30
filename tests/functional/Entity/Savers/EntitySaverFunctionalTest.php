1554300b3e01002634ef9266dee8c77a9ec0f48d
Joseph Edmonds <joseph@edmondscommerce.co.uk>
2018-05-30 07:57:16 +0000

    work in progress

---
+++ tests/functional/Entity/Savers/EntitySaverFunctionalTest.php
@@ -0,0 +1,76 @@
+<?php declare(strict_types=1);
+
+namespace EdmondsCommerce\DoctrineStaticMeta\Entity\Savers;
+
+use EdmondsCommerce\DoctrineStaticMeta\AbstractFunctionalTest;
+use EdmondsCommerce\DoctrineStaticMeta\AbstractIntegrationTest;
+use EdmondsCommerce\DoctrineStaticMeta\Entity\Interfaces\EntityInterface;
+use EdmondsCommerce\DoctrineStaticMeta\MappingHelper;
+
+class EntitySaverFunctionalTest extends AbstractFunctionalTest
+{
+    public const WORK_DIR = AbstractIntegrationTest::WORK_DIR.'/'.self::TEST_TYPE.'/EntitySaverFunctionalTest';
+
+    private const TEST_ENTITIES = [
+        self::TEST_PROJECT_ROOT_NAMESPACE.'\\Entities\\TestEntityOne',
+        self::TEST_PROJECT_ROOT_NAMESPACE.'\\Entities\\Deeply\\Nested\\TestEntityTwo',
+    ];
+
+    private const TEST_FIELDS = [
+        self::TEST_PROJECT_ROOT_NAMESPACE.'\\Entity\\Fields\\Traits\\NameFieldTrait',
+        self::TEST_PROJECT_ROOT_NAMESPACE.'\\Entity\\Fields\\Traits\\FooFieldTrait',
+    ];
+
+    public function setup()
+    {
+        parent::setup();
+        $fieldGenerator = $this->getFieldGenerator();
+        foreach (self::TEST_FIELDS as $fieldFqn) {
+            $fieldGenerator->generateField($fieldFqn, MappingHelper::TYPE_STRING);
+        }
+        $entityGenerator = $this->getEntityGenerator();
+        foreach (self::TEST_ENTITIES as $entityFqn) {
+            $entityGenerator->generateEntity($entityFqn);
+            foreach (self::TEST_FIELDS as $fieldFqn) {
+                $fieldGenerator->setEntityHasField($entityFqn, $fieldFqn);
+            }
+        }
+        $this->setupCopiedWorkDirAndCreateDatabase();
+    }
+
+    /**
+     * @param EntityInterface $entity
+     *
+     * @return EntitySaverInterface
+     * @throws \EdmondsCommerce\DoctrineStaticMeta\Exception\DoctrineStaticMetaException
+     */
+    private function getEntitySaver(EntityInterface $entity): EntitySaverInterface
+    {
+        /**
+         * @var EntitySaverFactory $entitySaverFactory
+         */
+        $entitySaverFactory = $this->container->get(EntitySaverFactory::class);
+
+        return $entitySaverFactory->getSaverForEntity($entity);
+
+    }
+
+    protected function loadFirstEntity(string $entityFqn)
+    {
+        $entityManager = $this->getEntityManager();
+
+        return $entityManager->getRepository($entityFqn)->findAll()[0];
+    }
+
+
+    public function testItCanSaveAndRemoveASingleEntity()
+    {
+        $entityFqn = current(self::TEST_ENTITIES);
+        $entity    = new $entityFqn();
+        $entity->setName('blah');
+        $entity->setfoo('bar');
+        $saver = $this->getEntitySaver($entity);
+        $saver->save($entity);
+
+    }
+}
