20e339e24ff116bbb285972da37155d71d3b7a50
Joseph Edmonds <joseph@edmondscommerce.co.uk>
2018-05-30 12:13:26 +0000

    making codacy happy

--- qaConfig/qaConfig.inc.bash
+++ qaConfig/qaConfig.inc.bash
@@ -2,7 +2,7 @@
 # Infection Configs
 
 ## Due to requiring to track random values, we can only run tests in a single thread
-numberOfCores=1
+export numberOfCores=1
 
 ## Totally Disabling Infection for now
-useInfection=0
+export useInfection=0
