387a4c5df8111c06ffd14996d37faafa3979e7a5
Joseph Edmonds <joseph@edmondscommerce.co.uk>
2018-05-31 11:07:59 +0000

    some test coverage on the exceptions

---
+++ tests/unit/Exception/Traits/RelativePathTraceTraitTest.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+
+namespace EdmondsCommerce\DoctrineStaticMeta\Exception\Traits;
+
+use PHPUnit\Framework\TestCase;
+
+class RelativePathTraceTraitTest extends TestCase
+{
+    public function testGetTraceAsStringRelativePath()
+    {
+        try {
+            throw new class extends \Exception
+            {
+                use RelativePathTraceTrait;
+            };
+        } catch (\Exception $e) {
+            $expected = '
+
+#0 /vendor/phpunit/phpunit/src/Framework/TestCase.php(1145): EdmondsCommerce\DoctrineStaticMeta\Exception\Traits\RelativePathTraceTraitTest->testGetTraceAsStringRelativePath()
+#1 /vendor/phpunit/phpunit/src/Framework/TestCase.php(840): PHPUnit\Framework\TestCase->runTest()
+#2 /vendor/phpunit/phpunit/src/Framework/TestResult.php(645): PHPUnit\Framework\TestCase->runBare()
+#3 /vendor/phpunit/phpunit/src/Framework/TestCase.php(798): PHPUnit\Framework\TestResult->run(Object(EdmondsCommerce\DoctrineStaticMeta\Exception\Traits\RelativePathTraceTraitTest))
+#4 /vendor/phpunit/phpunit/src/Framework/TestSuite.php(776): PHPUnit\Framework\TestCase->run(Object(PHPUnit\Framework\TestResult))
+#5 /vendor/phpunit/phpunit/src/TextUI/TestRunner.php(529): PHPUnit\Framework\TestSuite->run(Object(PHPUnit\Framework\TestResult))
+#6 /vendor/phpunit/phpunit/src/TextUI/Command.php(198): PHPUnit\TextUI\TestRunner->doRun(Object(PHPUnit\Framework\TestSuite), Array, true)
+#7 /vendor/phpunit/phpunit/src/TextUI/Command.php(151): PHPUnit\TextUI\Command->run(Array, true)
+#8 /vendor/phpunit/phpunit/phpunit(53): PHPUnit\TextUI\Command::main()
+#9 {main}
+
+';
+            $actual   = $e->getTraceAsStringRelativePath();
+            $this->assertSame($expected, $actual);
+        }
+    }
+}
---
+++ tests/unit/Exception/ValidationExceptionTest.php
@@ -0,0 +1,111 @@
+<?php declare(strict_types=1);
+
+namespace EdmondsCommerce\DoctrineStaticMeta\Exception;
+
+use Doctrine\ORM\Mapping\ClassMetadata as DoctrineClassMetaData;
+use EdmondsCommerce\DoctrineStaticMeta\Entity\Interfaces\EntityInterface;
+use EdmondsCommerce\DoctrineStaticMeta\Entity\Interfaces\Validation\EntityValidatorInterface;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Validator\ConstraintViolationList;
+use Symfony\Component\Validator\Mapping\ClassMetadata as ValidatorClassMetaData;
+
+
+class ValidationExceptionTest extends TestCase
+{
+    /**
+     * @var ValidationException
+     */
+    private $exception;
+
+    private $errors;
+
+    private $entity;
+
+    public function setup()
+    {
+        try {
+            $this->errors = new ConstraintViolationList();
+            $this->entity = new class implements EntityInterface
+            {
+                public function getId()
+                {
+                    // TODO: Implement getId() method.
+                }
+
+                public static function loadMetadata(DoctrineClassMetaData $metadata): void
+                {
+                    // TODO: Implement loadMetadata() method.
+                }
+
+                public static function getPlural(): string
+                {
+                    return '';
+                }
+
+                public static function getSingular(): string
+                {
+                    return '';
+                }
+
+                public static function getIdField(): string
+                {
+                    return '';
+                }
+
+                public function getShortName(): string
+                {
+                    return '';
+                }
+
+                public function __toString(): string
+                {
+                    return '';
+                }
+
+                public static function loadValidatorMetaData(ValidatorClassMetaData $metadata): void
+                {
+                    // TODO: Implement loadValidatorMetaData() method.
+                }
+
+                public function injectValidator(EntityValidatorInterface $validator)
+                {
+                    // TODO: Implement injectValidator() method.
+                }
+
+                public function isValid(): bool
+                {
+                    return false;
+                }
+
+                public function validate()
+                {
+                    // TODO: Implement validate() method.
+                }
+
+                public function validateProperty(string $propertyName)
+                {
+                    // TODO: Implement validateProperty() method.
+                }
+
+
+            };
+            throw new ValidationException($this->errors, $this->entity);
+        } catch (ValidationException $e) {
+            $this->exception = $e;
+        }
+    }
+
+    public function testGetInvalidEntity()
+    {
+        $expected = $this->entity;
+        $actual   = $this->exception->getInvalidEntity();
+        $this->assertSame($expected, $actual);
+    }
+
+    public function testGetValidationErrors()
+    {
+        $expected = $this->errors;
+        $actual   = $this->exception->getValidationErrors();
+        $this->assertSame($expected, $actual);
+    }
+}
