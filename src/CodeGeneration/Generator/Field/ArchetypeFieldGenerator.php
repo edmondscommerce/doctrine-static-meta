c90b895c688ad4798e26efe5120af0ed1bfa824e
Joseph Edmonds <joseph@edmondscommerce.co.uk>
2018-06-29 17:42:11 +0000

    line was too long

--- src/CodeGeneration/Generator/Field/ArchetypeFieldGenerator.php
+++ src/CodeGeneration/Generator/Field/ArchetypeFieldGenerator.php
@@ -205,7 +205,8 @@ class ArchetypeFieldGenerator
         $archetypePropertyName = $this->getPropertyName($this->archetypeFieldTrait->getShortName());
         $fieldPropertyName     = $this->getPropertyName($this->namespaceHelper->getClassShortName($this->fieldFqn));
         $find                  = [
-            '%(namespace|use) +?'.$this->findAndReplaceHelper->escapeSlashesForRegex($this->getArchetypeFqnRoot()).'(?!\\\\FakerData\\\\Abstract)%',
+            '%(namespace|use) +?'.$this->findAndReplaceHelper->escapeSlashesForRegex($this->getArchetypeFqnRoot())
+            .'(?!\\\\FakerData\\\\Abstract)%',
             '%'.$this->findAndReplaceHelper->escapeSlashesForRegex($this->getArchetypeSubNamespace()).'%',
             '%'.$this->codeHelper->classy($archetypePropertyName).'%',
             '%'.$this->codeHelper->consty($archetypePropertyName).'%',
