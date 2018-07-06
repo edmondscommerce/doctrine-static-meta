b76d43cc7147abc1d656dbded9d3c45719055a91
Joseph Edmonds <joseph@edmondscommerce.co.uk>
2018-07-06 15:56:49 +0000

    try giving me an invalid locale now, you caaant

--- src/Entity/Fields/FakerData/String/LocaleIdentifierFakerDataProvider.php
+++ src/Entity/Fields/FakerData/String/LocaleIdentifierFakerDataProvider.php
@@ -3,20 +3,53 @@
 namespace EdmondsCommerce\DoctrineStaticMeta\Entity\Fields\FakerData\String;
 
 use EdmondsCommerce\DoctrineStaticMeta\Entity\Fields\FakerData\AbstractFakerDataProvider;
+use Faker\Generator;
+use Symfony\Component\Intl\Intl;
 
 class LocaleIdentifierFakerDataProvider extends AbstractFakerDataProvider
 {
-    public const EXCLUDED_LOCALES = [
-        'trv_TW',
-        'gsw_CH',
-    ];
+
+    /**
+     * @var \Symfony\Component\Intl\ResourceBundle\LocaleBundleInterface
+     */
+    private static $localeBundle;
+    /**
+     * @var string[]
+     */
+    private static $locales;
+
+    /**
+     * LocaleIdentifierFakerDataProvider constructor.
+     *
+     * @param Generator $generator
+     * @SuppressWarnings(PHPMD.StaticAccess)
+     */
+    public function __construct(Generator $generator)
+    {
+        parent::__construct($generator);
+        if (null === self::$locales) {
+            self::$localeBundle = Intl::getLocaleBundle();
+            self::$locales      = self::$localeBundle->getLocaleNames();
+        }
+    }
+
+    private function isValid(string $value): bool
+    {
+        if (!isset(self::$locales[$value]) && !\in_array($value, self::$localeBundle->getAliases(), true)) {
+            return false;
+        }
+
+        return true;
+    }
 
     public function __invoke(): string
     {
         do {
-            $return = $this->generator->locale;
-        } while (\in_array($return, self::EXCLUDED_LOCALES, true));
+            $value = $this->generator->locale;
+        } while (false === $this->isValid($value));
 
-        return $return;
+        return $value;
     }
 }
+
+
