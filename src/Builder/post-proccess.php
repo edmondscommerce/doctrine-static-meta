c00edb0f239321fcafc663fb1a9734c2c5e8afac
Ross Mitchell <ross@edmondscommerce.co.uk>
2018-08-02 14:20:31 +0100

    Adding a basic builder for post processing

---
+++ src/Builder/post-proccess.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+namespace EdmondsCommerce\VaultEntities\Build;
+
+use EdmondsCommerce\DoctrineStaticMeta\SimpleEnv;
+use EdmondsCommerce\VaultEntities\Entities\Portfolio\Holding;
+use EdmondsCommerce\VaultEntities\Entities\PortfolioTest;
+use EdmondsCommerce\VaultEntities\Entity\Interfaces\Portfolio\HoldingInterface;
+use EdmondsCommerce\VaultEntities\Entity\Relations\Commodity\Traits\HasCommodity\HasCommodityManyToOne;
+use EdmondsCommerce\VaultEntities\Entity\Relations\Commodity\Traits\HasCommodity\HasCommodityUnidirectionalManyToOne;
+use EdmondsCommerce\VaultEntities\Entity\Relations\Portfolio\Traits\HasPortfolio\HasPortfolioManyToOne;
+use EdmondsCommerce\VaultEntities\Extensions\Entities\Portfolio\AvoidRemovingRequiredAssociationsTrait;
+use EdmondsCommerce\VaultEntities\Extensions\Entities\Portfolio\Holding\CompositeIdField;
+use EdmondsCommerce\VaultEntities\Extensions\Entity\Fields\Interfaces\PrimaryKey\CompositeIdFieldInterface;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+SimpleEnv::setEnv(__DIR__ . '/../.env');
+$containerBuilder = new \Symfony\Component\DependencyInjection\ContainerBuilder();
+$containerBuilder->autowire(Builder::class)->setPublic(true);
+(new \EdmondsCommerce\DoctrineStaticMeta\Container())->addConfiguration($containerBuilder, $_SERVER);
+$containerBuilder->compile();
+
+/** @var Builder $builder */
+$builder = $containerBuilder->get(Builder::class);
+
+/* You can now use the builder process the classes */
