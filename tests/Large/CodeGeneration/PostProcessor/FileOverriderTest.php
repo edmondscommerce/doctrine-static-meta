80709adba1a85b3b1cfc7a2b3c92c55aa5a9c6fd
Joseph Edmonds <joseph@edmondscommerce.co.uk>
2018-09-09 16:32:57 +0000

    fixing namespace

--- tests/Large/CodeGeneration/PostProcessor/FileOverriderTest.php
+++ tests/Large/CodeGeneration/PostProcessor/FileOverriderTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace EdmondsCommerce\DoctrineStaticMeta\Tests\Small\CodeGeneration\PostProcessor;
+namespace EdmondsCommerce\DoctrineStaticMeta\Tests\Large\CodeGeneration\PostProcessor;
 
 use EdmondsCommerce\DoctrineStaticMeta\CodeGeneration\PostProcessor\FileOverrider;
 use EdmondsCommerce\DoctrineStaticMeta\Tests\Assets\AbstractTest;
