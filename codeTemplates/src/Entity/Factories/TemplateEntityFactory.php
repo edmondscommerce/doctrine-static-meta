2903649cbe6df01b2bd8c6c0ee03936656844433
Joseph Edmonds <joseph@edmondscommerce.co.uk>
2018-09-26 16:19:35 +0000

    specific entity factories now have the DTO

--- codeTemplates/src/Entity/Factories/TemplateEntityFactory.php
+++ codeTemplates/src/Entity/Factories/TemplateEntityFactory.php
@@ -3,14 +3,15 @@
 namespace TemplateNamespace\Entity\Factories;
 
 // phpcs:disable -- line length
-use TemplateNamespace\Entity\Factories\AbstractEntityFactory;
 use TemplateNamespace\Entities\TemplateEntity;
+use TemplateNamespace\Entity\DataTransferObjects\TemplateEntityDto;
 use TemplateNamespace\Entity\Interfaces\TemplateEntityInterface;
+
 // phpcs: enable
 class TemplateEntityFactory extends AbstractEntityFactory
 {
-    public function create(array $values = []): TemplateEntityInterface
+    public function create(TemplateEntityDto $dto = null): TemplateEntityInterface
     {
-        return $this->entityFactory->create(TemplateEntity::class, $values);
+        return $this->entityFactory->create(TemplateEntity::class, $dto);
     }
 }
