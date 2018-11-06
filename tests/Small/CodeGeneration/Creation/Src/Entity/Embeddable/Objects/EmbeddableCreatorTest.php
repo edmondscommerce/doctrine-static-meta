8f02b5c05e695f1b2b4dd18cca5eae9517e1c2c0
Joseph Edmonds <joseph@edmondscommerce.co.uk>
2018-11-06 17:04:28 +0000

    fixing test coverage

--- tests/Small/CodeGeneration/Creation/Src/Entity/Embeddable/Objects/EmbeddableCreatorTest.php
+++ tests/Small/CodeGeneration/Creation/Src/Entity/Embeddable/Objects/EmbeddableCreatorTest.php
@@ -11,6 +11,10 @@ use EdmondsCommerce\DoctrineStaticMeta\Config;
 use EdmondsCommerce\DoctrineStaticMeta\Tests\Small\ConfigTest;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * @small
+ * @covers \EdmondsCommerce\DoctrineStaticMeta\CodeGeneration\Creation\Src\Entity\Embeddable\Objects\EmbeddableCreator
+ */
 class EmbeddableCreatorTest extends TestCase
 {
     private const EXPECTED_FILE = <<<'PHP'
