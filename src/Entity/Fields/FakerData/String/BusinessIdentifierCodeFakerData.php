825c8861e98203d072add2ab3534643d1342c344
Joseph Edmonds <joseph@edmondscommerce.co.uk>
2019-01-02 14:44:13 +0000

    totally replaced the swiftCode generator with something that uses valid country codes

--- src/Entity/Fields/FakerData/String/BusinessIdentifierCodeFakerData.php
+++ src/Entity/Fields/FakerData/String/BusinessIdentifierCodeFakerData.php
@@ -7,15 +7,35 @@ use Faker\Generator;
 
 class BusinessIdentifierCodeFakerData extends AbstractFakerDataProvider
 {
+    /**
+     * @var CountryCodeFakerData
+     */
+    private $countryCode;
 
     public function __construct(Generator $generator)
     {
         parent::__construct($generator);
+        $this->countryCode = new CountryCodeFakerData($generator);
     }
 
 
     public function __invoke(): string
     {
-        return $this->generator->swiftBicNumber;
+        return $this->getBank() . $this->getCountryCode() . $this->getRegionAndBranch();
+    }
+
+    private function getBank(): string
+    {
+        return $this->generator->regexify('[A-Z]{4}');
+    }
+
+    private function getCountryCode(): string
+    {
+        return $this->countryCode->__invoke();
+    }
+
+    private function getRegionAndBranch(): string
+    {
+        return $this->generator->regexify('^([0-9A-Z]){2}([0-9A-Z]{3})?$');
     }
 }
