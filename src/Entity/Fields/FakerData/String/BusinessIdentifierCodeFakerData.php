0945a2cd3f7b2936631a4fcfdf5a6bbc63ddbec4
Joseph Edmonds <joseph@edmondscommerce.co.uk>
2019-01-02 14:05:07 +0000

    switching to suggested `fromShellCommandLine` to resolve error raised in Symfony ProcessPassing a command as string when creating a "Symfony\Component\Process\Process" instance is deprecated since Symfony 4.2, pass it as an array of its arguments instead, or use the "Process::fromShellCommandline()" constructor if you need features provided by the shell.

--- src/Entity/Fields/FakerData/String/BusinessIdentifierCodeFakerData.php
+++ src/Entity/Fields/FakerData/String/BusinessIdentifierCodeFakerData.php
@@ -3,9 +3,17 @@
 namespace EdmondsCommerce\DoctrineStaticMeta\Entity\Fields\FakerData\String;
 
 use EdmondsCommerce\DoctrineStaticMeta\Entity\Fields\FakerData\AbstractFakerDataProvider;
+use Faker\Generator;
 
 class BusinessIdentifierCodeFakerData extends AbstractFakerDataProvider
 {
+
+    public function __construct(Generator $generator)
+    {
+        parent::__construct($generator);
+    }
+
+
     public function __invoke(): string
     {
         return $this->generator->swiftBicNumber;
