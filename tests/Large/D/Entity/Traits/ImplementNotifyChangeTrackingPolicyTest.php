0d6ad620a2d90a6efb00849724a229d22c900782
joseph <info@edmondscommerce.co.uk>
2019-04-08 16:49:27 +0100

    fixing namespace in test

--- tests/Large/D/Entity/Traits/ImplementNotifyChangeTrackingPolicyTest.php
+++ tests/Large/D/Entity/Traits/ImplementNotifyChangeTrackingPolicyTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace EdmondsCommerce\DoctrineStaticMeta\Tests\Large\Entity\Traits;
+namespace EdmondsCommerce\DoctrineStaticMeta\Tests\Large\D\Entity\Traits;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
