fb8398c8ac75ce9c3b13fe00c5e6ce2b6bafa0eb
joseph <info@edmondscommerce.co.uk>
2019-05-21 13:58:46 +0100

    getting xdebug config - debugging travis issue

--- ci.bash
+++ ci.bash
@@ -11,6 +11,11 @@ echo "
 $(hostname) $0 $@
 ===========================================
 "
+
+echo "Xdebug config:"
+php -i | grep xdebug
+
+echo "Setting up DB"
 dbUser=dsm
 dbPass=dsm
 dbName=dsm
@@ -34,17 +39,21 @@ mysql -e "CREATE DATABASE IF NOT EXISTS $dbName CHARACTER SET utf8 COLLATE utf8_
 mysql -e "GRANT ALL PRIVILEGES on *.* TO '$dbUser'@'$dbHost' IDENTIFIED BY '$dbPass' WITH GRANT OPTION;"
 mysql -e "FLUSH PRIVILEGES;"
 mysql -e "SHOW GRANTS FOR '$dbUser'@'$dbHost'"
+echo "Done"
 
+echo "Creating directories:"
 mkdir -p $DIR/cache/Proxies && chmod 777 $DIR/cache/Proxies
 mkdir -p $DIR/cache/qa && chmod 777 $DIR/cache/qa
+echo "Done"
 
+echo "Running QA Pipeline"
 export phpUnitQuickTests=${phpUnitQuickTests:-0}
 export phpUnitCoverage=${phpUnitCoverage:-0}
 export phpUnitTestsSubDir=${phpUnitTestsSubDir:-''}
 
 #Run the QA command, defaults to bin/qa
 bash -c "${qaCmd:-bin/qa}"
-
+echo "Done"
 
 
 echo "
