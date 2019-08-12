651c0086bcb4940cee1be5c4b854ffdb15c870db
joseph <info@edmondscommerce.co.uk>
2019-08-12 11:04:27 +0100

    using import alias to ensure we dont remove required import

--- codeTemplates/src/Entity/Factories/TemplateEntityFactory.php
+++ codeTemplates/src/Entity/Factories/TemplateEntityFactory.php
@@ -5,10 +5,11 @@ namespace TemplateNamespace\Entity\Factories;
 // phpcs:disable -- line length
 use TemplateNamespace\Entities\TemplateEntity;
 use TemplateNamespace\Entity\DataTransferObjects\TemplateEntityDto;
+use TemplateNamespace\Entity\Factories\AbstractEntityFactory as ProjectAbstractEntityFactory;
 use TemplateNamespace\Entity\Interfaces\TemplateEntityInterface;
 
 // phpcs: enable
-class TemplateEntityFactory extends AbstractEntityFactory
+class TemplateEntityFactory extends ProjectAbstractEntityFactory
 {
     public function create(TemplateEntityDto $dto = null): TemplateEntityInterface
     {
