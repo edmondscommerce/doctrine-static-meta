66550ab4e66d39cafc5a6f75caf2ebd5f0f76449
Ross Mitchell <ross@edmondsommerce.co.uk>
2019-11-15 17:58:29 +0000

    Updating another method

--- src/Entity/Fields/FakerData/String/UnicodeLanguageIdentifierFakerData.php
+++ src/Entity/Fields/FakerData/String/UnicodeLanguageIdentifierFakerData.php
@@ -6,7 +6,10 @@ namespace EdmondsCommerce\DoctrineStaticMeta\Entity\Fields\FakerData\String;
 
 use EdmondsCommerce\DoctrineStaticMeta\Entity\Fields\FakerData\AbstractFakerDataProvider;
 use Faker\Generator;
+use RuntimeException;
 use Symfony\Component\Intl\Intl;
+use Symfony\Component\Intl\Languages;
+use function class_exists;
 
 class UnicodeLanguageIdentifierFakerData extends AbstractFakerDataProvider
 {
@@ -28,5 +31,23 @@ class UnicodeLanguageIdentifierFakerData extends AbstractFakerDataProvider
         } while (!isset($this->languages[$language]));
 
         return $language;
+    }
+
+    /**
+     * Using the updated language provider if it exists
+     *
+     * @return array
+     */
+    private function getLanguages(): array
+    {
+        if (class_exists(Languages::class)) {
+            return Languages::getNames();
+        }
+
+        if (class_exists(Intl::class)) {
+            return Intl::getLanguageBundle()->getLanguageNames();
+        }
+
+        throw new RuntimeException('No language provider exists');
     }
 }
